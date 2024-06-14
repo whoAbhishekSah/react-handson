37ffe18af08e0a443595d1731e51c0fac9574cc8
Abhishek Sah <abhi.sah.97@gmail.com>
2024-06-14 21:44:53 +0530

    fmt code

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,62 @@
 import { useEffect, useState } from "react";
 
-const UserBrief = ({user}) => {
-  return (<div><p>Name: {user.firstName} {user.lastName} </p><p>Phone Number: {user.phone}</p></div>)
-}
+const UserBrief = ({ user }) => {
+  return (
+    <div>
+      <p>
+        Name: {user.firstName} {user.lastName}{" "}
+      </p>
+      <p>Phone Number: {user.phone}</p>
+    </div>
+  );
+};
+
+const ErrorPage = ({ err }) => {
+  return (
+    <div>
+      <p>Error: {err}</p>
+    </div>
+  );
+};
 
 const Profile = ({ id }) => {
   const [user, setUser] = useState(undefined);
   const [isLoading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(undefined);
 
   useEffect(() => {
     const fetchUser = async () => {
-      try{
-        setIsLoading(true)
-        console.log("called fetch user")
+      try {
+        setIsLoading(true);
         const response = await fetch(`https://dummyjson.com/users/${id}`);
         const jsonData = await response.json();
         setUser(jsonData);
-      }
-      catch(err){
-        console.error(err)
-      }finally{
-        setIsLoading(false)
+      } catch (err) {
+        console.error(err);
+        setIsError(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
     fetchUser();
   }, [id]);
-  
+
   if (isLoading || !user) {
     return <div>Loading...</div>;
   }
 
-  return (
-    <UserBrief user={user} />
-  );
+  if (isError) {
+    return <ErrorPage err={isError} />;
+  }
+
+  return <UserBrief user={user} />;
 };
 
 function App() {
   return (
     <div className="App">
-      <Profile id={1}/>
+      <Profile id={"abs"} />
     </div>
   );
 }

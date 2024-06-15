1de43cff8bed534f5d79f21420fd681e7bdbdb09
Abhishek Sah <abhi.sah.97@gmail.com>
2024-06-15 19:33:31 +0530

    extract user profile in seperate module

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,5 @@
-import { useEffect } from 'react';
-import { useService } from './hooks/service';
-import { UserBrief } from './UserBrief';
-import { ErrorPage } from './Error';
+import { Profile } from './Profile';
 import { Friends } from './friends';
-
-const Profile = ({ id }) => {
-  const {
-    loading,
-    error,
-    data: user,
-    fetchUrl,
-  } = useService(`http://localhost:4000/api/users/${id}`);
-
-  useEffect(() => {
-    fetchUrl();
-  }, [id]);
-
-  if (error) {
-    return <ErrorPage err={`failed to fetch user with id ${id}`} />;
-  }
-
-  if (loading || !user) {
-    return <div>Loading...</div>;
-  }
-
-  return <UserBrief user={user} />;
-};
 
 function App() {
   return (

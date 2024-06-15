8f03f4a087103f2531bfbc2958b3958f0e593832
Abhishek Sah <abhi.sah.97@gmail.com>
2024-06-15 19:37:26 +0530

    fix few react warnings

--- src/friends.jsx
+++ src/friends.jsx
@@ -26,20 +26,24 @@ export const Friends = ({ id }) => {
     <div>
       <p>Friends List</p>
       <table>
-        <tr>
-          <th>Name</th>
-          <th>Phone</th>
-        </tr>
-        {friends.map((i) => {
-          return (
-            <tr>
-              <td>
-                {i['firstName']} {i['lastName']}
-              </td>
-              <td>{i['phone']}</td>
-            </tr>
-          );
-        })}
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Phone</th>
+          </tr>
+        </thead>
+        <tbody>
+          {friends.map((i) => {
+            return (
+              <tr>
+                <td>
+                  {i['firstName']} {i['lastName']}
+                </td>
+                <td>{i['phone']}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
